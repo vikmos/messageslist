0749ad6787ae974cc3f2223a95b3a0bb95d1882c
Viktor Moskalenko <tehb23@gmail.com>
2022-04-27 18:11:41 +0300

    Осталось оформить README.md

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
-def index_page(request: Request):
-    return templates.TemplateResponse("messages.html", {
+async def index_page(request: Request, ):
+    return templates.TemplateResponse("index.html", {
         "request": request
         })
 
-
-
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_json()
+            await websocket.send_json(data)
+    except WebSocketDisconnect:
+        print(f"\033[33mWARNING\033[37m:  \033[33mThe user reload or close the page")
