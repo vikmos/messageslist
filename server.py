53f1cacacfb3a73f189319373c71c5c7b93e9671
Viktor Moskalenko <tehb23@gmail.com>
2022-04-28 13:09:14 +0300

    Добавлены комментарии

--- server.py
+++ server.py
@@ -8,12 +8,14 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def index_page(request: Request, ):
+    """Функция возращает html страницу клиенту"""
     return templates.TemplateResponse("index.html", {
         "request": request
         })
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    """Функция получает и отправляет данные через веб-сокет соединение"""
     await websocket.accept()
     try:
         while True:
